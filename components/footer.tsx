2f98ab39e9e1ffc0272d57211840772ac367ef6a
KrlsMnl <44993290+KrlsMnl@users.noreply.github.com>
2026-07-20 20:46:51 -0400

    Fix Facebook page link

--- components/footer.tsx
+++ components/footer.tsx
@@ -228,7 +228,7 @@ export function Footer() {
               },
               {
                 label: "Facebook",
-                href: "https://www.facebook.com/amberprintstudio",
+                href: "https://www.facebook.com/profile.php?id=61588302980119",
                 icon: (
                   <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round">
                     <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" />
